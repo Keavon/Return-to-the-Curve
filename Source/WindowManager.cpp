a4b50a05fa0bdcf62ad915d946c0f3e034bd0d74
Keavon Chambers <keavon@keavon.com>
2019-05-30 12:49:24 -0700

    Add maximized window hint

--- Source/WindowManager.cpp
+++ Source/WindowManager.cpp
@@ -42,6 +42,7 @@ bool WindowManager::init(int const width, int const height)
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_MAXIMIZED, GL_TRUE);
 
 	// Create a windowed mode window and its OpenGL context.
 	windowHandle = glfwCreateWindow(width, height, "Return To the Curve", nullptr, nullptr);
