488c0a23b69ae683b328f1a6c62b52c10778f01b
cvirostek <cwvirostek@gmail.com>
2019-06-05 21:25:52 -0700

    Removed cmake temp files

--- build/yaml-cpp/test/gtest-1.8.0/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/gyatso/Documents/cpe476/CPE-476-Project-Curve/yaml-cpp/test/gtest-1.8.0/googlemock
-# Build directory: /home/gyatso/Documents/cpe476/CPE-476-Project-Curve/build/yaml-cpp/test/gtest-1.8.0/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
--- build/yaml-cpp/util/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/gyatso/Documents/cpe476/CPE-476-Project-Curve/yaml-cpp/util
-# Build directory: /home/gyatso/Documents/cpe476/CPE-476-Project-Curve/build/yaml-cpp/util
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
